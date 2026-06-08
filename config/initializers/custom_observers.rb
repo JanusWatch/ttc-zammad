dac6fa1fff4d139c487d30371b0e926e637daea4
Ubuntu <zammadadmin@soc-itsm-vm.qjdi5zy2fdluldunbqu0uqruva.ax.internal.cloudapp.net>
2026-06-08 12:25:40 +0000

    --amend

--- config/initializers/custom_observers.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.config.after_initialize do
-  require_dependency Rails.root.join('lib/custom_observer/ticket_company_observer.rb')
-  ActiveRecord::Base.observers << CustomObserver::TicketCompanyObserver
-  ActiveRecord::Base.instantiate_observers
-end
